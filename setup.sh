4c3ebccf3e60d5c74006529b8740c2f80774377f
Rajat Jindal <rajatjindal83@gmail.com>
2018-01-13 08:15:07 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 apt-get -y update
 apt-get -y install golang-go
 mkdir /opt && cd /opt
-git clone git@github.com:rajatjindal/custom-controller.git
+git clone https://github.com/rajatjindal/custom-controller.git
 cd custom-controller
 go run main.go
